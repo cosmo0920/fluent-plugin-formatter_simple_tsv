e0c7a80e1514d48635c13eb7e9fbae913c2bd600
Hiroshi Hatake <hatake@clear-code.com>
2015-08-10 17:00:25 +0900

    Use Fluent::Config::Element for making config values

--- test/test_formatter_simple_tsv.rb
+++ test/test_formatter_simple_tsv.rb
@@ -9,9 +9,17 @@ class SimpleTsvFormatterTest < ::Test::Unit::TestCase
     @tag = 'tag'
   end
 
+  def e(name, arg='', attrs={}, elements=[])
+    Fluent::Config::Element.new(name, arg, attrs, elements)
+  end
+
+  def configure(conf)
+    @formatter.configure(e('ROOT', '', {}.merge(conf)))
+  end
+
   def test_format_empty_configure
     assert_raise Fluent::ConfigError do
-      @formatter.configure({})
+      configure({})
     end
   end
 
@@ -19,16 +27,15 @@ class SimpleTsvFormatterTest < ::Test::Unit::TestCase
     formatter = Fluent::Plugin.new_formatter('simple_tsv')
     assert_equal([], formatter.keys)
 
-    formatter.configure(
-      'keys'      => 'key1,key2',
-    )
+    conf = e('ROOT', '', {'keys' => 'key1,key2'})
+    formatter.configure(conf)
 
     assert_equal(["key1", "key2"], formatter.keys)
   end
 
 
   def test_format
-    @formatter.configure('keys' => 'key1,key2')
+    configure('keys' => 'key1,key2')
     formatted = @formatter.format(@tag, @time, {
                                     'key1' => 'awesome',
                                     'key2' => 'awesome2'
@@ -37,7 +44,7 @@ class SimpleTsvFormatterTest < ::Test::Unit::TestCase
   end
 
   def test_format_keys
-    @formatter.configure('keys' => 'key1,key3')
+    configure('keys' => 'key1,key3')
     formatted = @formatter.format(@tag, @time, {
                                     'key1' => 'awesome',
                                     'key2' => 'awesome2',
@@ -47,9 +54,9 @@ class SimpleTsvFormatterTest < ::Test::Unit::TestCase
   end
 
   def test_format_with_time
-    @formatter.configure('keys' => 'key1,key3',
-                         'include_time_key' => 'true',
-                         'time_format' => '%Y')
+    configure('keys' => 'key1,key3',
+              'include_time_key' => 'true',
+              'time_format' => '%Y')
     formatted = @formatter.format(@tag, @time, {
                                     'key1' => 'awesome',
                                     'key2' => 'awesome2',
@@ -59,8 +66,8 @@ class SimpleTsvFormatterTest < ::Test::Unit::TestCase
   end
 
   def test_format_with_tag
-    @formatter.configure('keys' => 'key1,key3',
-                         'include_tag_key' => 'true')
+    configure('keys' => 'key1,key3',
+              'include_tag_key' => 'true')
     formatted = @formatter.format(@tag, @time, {
                                     'key1' => 'awesome',
                                     'key2' => 'awesome2',
@@ -70,10 +77,10 @@ class SimpleTsvFormatterTest < ::Test::Unit::TestCase
   end
 
   def test_format_with_tag_and_time
-    @formatter.configure('keys' => 'key1,key3',
-                         'include_tag_key' => 'true',
-                         'include_time_key' => 'true',
-                         'time_format' => '%Y')
+    configure('keys' => 'key1,key3',
+              'include_tag_key' => 'true',
+              'include_time_key' => 'true',
+              'time_format' => '%Y')
     formatted = @formatter.format(@tag, @time, {
                                     'key1' => 'awesome',
                                     'key2' => 'awesome2',
